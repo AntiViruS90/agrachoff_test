a39a3fad94a7bfdfc1848d7fd661c7891b76b411
AntiViruS90 <131804906+AntiViruS90@users.noreply.github.com>
2023-11-30 19:12:01 +0300

    Адаптив

--- js.js
+++ js.js
@@ -1,40 +1,36 @@
-$(document).ready(function() {
-    $('.menu_btn').click(function () {
-        $('.dropdownBlock').fadeIn(1500)
-    })
-    $('.close_btn').click(function (){
-        $('.dropdownBlock').fadeOut(1500)
-    })
-    $(window).scroll(function() {
-        var scrollX = $(window).scrollX();
-        $('.corner_img').css('transform', 'translateX(' + scrollX + 'px)');
-    });
-    
-    $('.footer_btn_email').click(function () {
-        $('.popup').fadeIn(500);
-    })
-    $('.footer_input_submit').click(function () {
-        $('.popup').fadeOut(100);
-    })
 
+const element = document.querySelector('.element');
+const parent = document.querySelector('.parent');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+parent.addEventListener('wheel', function(event) {
+if (event.deltaY < 0) {
+    element.classList.remove('active');
+} else if (event.deltaY > 0) {
+    element.classList.add('active');
+}
 });
+$('.menu_btn').click(function () {
+    $('.dropdownBlock').fadeIn(500)
+})
+$('.close_btn').click(function (){
+    $('.dropdownBlock').fadeOut(500)
+})
+$('.footer_btn_email').click(function () {
+    $('.popup').fadeIn(500);
+});
+
+$('.footer_input_submit').click(function () { 
+    if ($('#email').val() !== '') {         
+        $('.popup').fadeOut();
+        alert('Сообщение успешно отправлено!');
+    } else { 
+        alert('Данные для обратной связи отсутствуют!') }
+        $('.popup').fadeOut(2000);
+});
+
+$('.close_btn_sidebar').fadeOut()
+$('.dropdownBlock').fadeOut()
+
+
 
 
